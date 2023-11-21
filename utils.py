a978a6153081d634c30feabeb1789a7552785e9d
Deqformer <542576001@qq.com>
2023-11-21 22:47:07 +0800

    add file

--- utils.py
+++ utils.py
@@ -35,19 +35,18 @@ def clean_dataset(dataset, tokenizer):
     tokenized_datasets.set_format("torch")
     return tokenized_datasets
 
-def train_tokenizer(train_data, test_data):
-    tokenizer = Tokenizer(WordLevel(unk_token='[UNK]'))
-    tokenizer.pre_tokenizer = PreTokenizer.custom(DNAPreTokenizer())
-    trainer = WordLevelTrainer(vocab_size=256, special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
-    data= [e['text'] for e in train_data]
-    data.extend([e['text'] for e in test_data])
-    data.extend([e['rev_text'] for e in train_data])
-    data.extend([e['rev_text'] for e in test_data])
-    tokenizer.train_from_iterator(data, trainer=trainer)
-    return tokenizer
+# def train_tokenizer(train_data, test_data):
+#     tokenizer = Tokenizer(WordLevel(unk_token='[UNK]'))
+#     tokenizer.pre_tokenizer = PreTokenizer.custom(DNAPreTokenizer())
+#     trainer = WordLevelTrainer(vocab_size=256, special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
+#     data= [e['text'] for e in train_data]
+#     data.extend([e['text'] for e in test_data])
+#     data.extend([e['rev_text'] for e in train_data])
+#     data.extend([e['rev_text'] for e in test_data])
+#     tokenizer.train_from_iterator(data, trainer=trainer)
+#     return tokenizer
 
-def tokenization_dataset(train_data, test_data):
-    tokenizers = train_tokenizer(train_data, test_data)
+def tokenization_dataset(train_data, test_data, tokenizers):
 
     full_train_dataset = clean_dataset(train_data, tokenizers)
     full_eval_dataset = clean_dataset(test_data, tokenizers)
@@ -55,3 +54,49 @@ def tokenization_dataset(train_data, test_data):
     train_dataloader = DataLoader(full_train_dataset, shuffle=True, batch_size=16)
     eval_dataloader = DataLoader(full_eval_dataset, batch_size=16)
     return (train_dataloader, eval_dataloader)
+
+def train_tokenizer(train_datas, test_datas):
+    tokenizer = Tokenizer(WordLevel(unk_token='[UNK]'))
+    tokenizer.pre_tokenizer = PreTokenizer.custom(DNAPreTokenizer())
+    trainer = WordLevelTrainer(vocab_size=256, special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
+    #print(f'train_data is {train_data.next()}')
+    for (train_data, test_data) in zip(train_datas, test_datas):
+        data= [e['text'] for e in train_data]
+        # data.extend([e['text'] for e in test_data])
+        data.extend([e['rev_text'] for e in train_data])
+        #data.extend([e['rev_text'] for e in test_data])
+        tokenizer.train_from_iterator(data, trainer=trainer)
+    return tokenizer
+
+def save_result(model: torch.nn.Module, train_dataloader, opt, load_model, epoch, tokenzier) -> float:
+    des_dir = f'{opt.log_dir}/result.csv'
+    train_dataloader = train_dataloader
+    if load_model:
+        #des_dir = f'{opt.log_dir}/{epoch}.csv'
+        model.load_state_dict(torch.load(f"{opt.log_dir}/transformer"))
+    model.eval()  # turn on evaluation mode
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    with torch.no_grad():
+        with open(des_dir, 'a+', newline='') as csv_file:
+            writer = csv.writer(csv_file,delimiter=',')
+            for batch in train_dataloader:
+                batch = {k: v.to(device) for k, v in batch.items()}
+
+                #print(f'batch is {batch}')
+                outputs = model(**batch)
+                for e in range(outputs.logits.shape[0]):
+                    pred = outputs.logits[e].item()
+                    real = batch["labels"][e].item()
+                    id = batch["input_ids"][e].tolist()
+                    id = tokenzier.decode(id).split()
+                    # mean = 6.599401777846243
+                    # std = 1.2772261792750987
+                    std = 0.41257507475041016 
+                    mean =  2.7706192889195442
+                    pred = pred * std + mean
+                    real = real * std + mean
+                    writer.writerow([pred , real])
+                    #print(outputs.logits[e].item(), batch["labels"][e].item())
+                #break
+    model.train()
+    
